2dee62831f2524a02121b8c17ad9da892d5cbcd5
Roberto Villon <chrvillon@espol.edu.ec>
2025-07-04 17:21:05 -0500

    cambio del layout y mostrar mensajes

--- SIEM.c
+++ SIEM.c
@@ -5,127 +5,152 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <stdbool.h>
 
 #define BUFFER_SIZE 2048
+#define MAX_SERVICES 10
+#define MAX_LOGS 1000
 
 const char *all_priorities[] = {"emerg", "alert", "crit", "err", "warning", "notice", "info", "debug"};
 
-int priority_level(char* name)
-{
-    if (strcmp(name, "emerg") == 0) return 0;
-    if (strcmp(name, "alert") == 0) return 1;
-    if (strcmp(name, "crit") == 0) return 2;
-    if (strcmp(name, "err") == 0) return 3;
-    if (strcmp(name, "warning") == 0) return 4;
-    if (strcmp(name, "notice") == 0) return 5;
-    if (strcmp(name, "info") == 0) return 6;
-    if (strcmp(name, "debug") == 0) return 7;
-    return -1;
+char* logs[MAX_SERVICES][MAX_LOGS];
+int log_counts[MAX_SERVICES] = {0};
+
+bool pflag = false;
+char* priority = "";
+
+void clear_screen() {
+    printf("\033[H\033[J");
 }
 
-void nameset_input_nonblocking()
-{
-    int flags = fcntl(STDIN_FILENO, F_GETFD, 0);
-    fcntl(STDIN_FILENO, F_SETFD, flags | O_NONBLOCK);
+void print_help(char *command) {
+    printf("Uso:\n %s <nombre_servicio_1.service> <nombre_servicio_2.service> ... [-p <nilvel de prioridad>] <Tiempo_de_actualizacion_en_segundos>\n", command);
+    printf("Opciones\n");
+    printf("-p\t\tMuestra los mensajes con con el nivel pripridad indicado (emerg, alert, crit, err, notice, info, debug).\n");
+    printf("Aviso: Si no se especifica el tipo de prioridad solo se mostrara los mensajes de los ultimos 15 min.\n");
 }
 
 int main(int argc, char **argv)
 {
+    int opt; 
+
+    while ((opt = getopt(argc, argv, "p:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            pflag = true;
+            priority = optarg;
+            break;
+        case 'h':
+            print_help(argv[0]);
+            return 0;
+        default:
+            printf("Uso:\n %s <nombre_servicio_1.service> <nombre_servicio_2.service> ... [-p <nilvel de prioridad>] <Actualizacion_en_segundos>\n", argv[0]);
+            printf(" %s -h\n", argv[0]);
+            return 1;
+        }
+    }
+    
+
     int time = atoi(argv[argc - 1]);
-    int num_services = argc - 2;
+    int num_services = argc - optind - 1;   
+    int interval = atoi(argv[argc - 1]);
     pid_t pid;
-    int pipes[num_services][2];
 
     if (argc < 4)
-    {
-        fprintf(stderr, "Uso: %s servicio1 [servicio2 ...] intervalo_segundos\n", argv[0]);
+    {        fprintf(stderr, "Uso: %s servicio1 [servicio2 ...] intervalo_segundos\n", argv[0]);
         exit(1);
+        print_help(argv[0]);
     }
 
-    nameset_input_nonblocking();
+    clear_screen();
+    printf("╔═══════════════════════╦════════════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║ Servicio              ║ Conteo por prioridad                                                               ║\n");
+    printf("╠═══════════════════════╬════════════════════════════════════════════════════════════════════════════════════╣\n");
 
     for (int i = 0; i < num_services; i++)
     {
-        pipe(pipes[i]);
-        pid = fork();
+        int pipefd[2];
+        pipe(pipefd);
 
+        pid = fork();
         if (pid == 0)
         {
-            close(pipes[i][0]);
-            dup2(pipes[i][1], STDOUT_FILENO);
-            close(pipes[i][1]);
+            close(pipefd[0]);
+            dup2(pipefd[1], STDOUT_FILENO);
+            close(pipefd[1]);
 
             execlp("journalctl", "journalctl", "-u", argv[i + 1], "--output=json", NULL);
             perror("execlp");
             exit(1);
         }
 
-        close(pipes[i][1]);
-    }
+        close(pipefd[1]);
 
-    for (int i = 0; i < num_services; i++) {
-        FILE *fp = fdopen(pipes[i][0], "r");
+        FILE *fp = fdopen(pipefd[0], "r");
+
         if (!fp) {
             perror("fdopen");
             continue;
         }
-    
-        int emerg = 0, alert = 0, crit = 0, err = 0;
-        int warning = 0, notice = 0, info = 0, debug = 0;
-    
+
+        int priority_number[8] = {0};
         char line[BUFFER_SIZE];
-        char priority_input[16] = "";
-        int priority = -1;
-    
-        char input[32];
-        if (fgets(input, sizeof(input), stdin)) {
-            input[strcspn(input, "\n")] = 0;
-            strcpy(priority_input, input);
-            priority = priority_level(priority_input);
-        }
-    
+
         while (fgets(line, sizeof(line), fp))
         {
-            if (strstr(line, "\"PRIORITY\":\"0\"")) emerg++;
-            else if (strstr(line, "\"PRIORITY\":\"1\"")) alert++;
-            else if (strstr(line, "\"PRIORITY\":\"2\"")) crit++;
-            else if (strstr(line, "\"PRIORITY\":\"3\"")) err++;
-            else if (strstr(line, "\"PRIORITY\":\"4\"")) warning++;
-            else if (strstr(line, "\"PRIORITY\":\"5\"")) notice++;
-            else if (strstr(line, "\"PRIORITY\":\"6\"")) info++;
-            else if (strstr(line, "\"PRIORITY\":\"7\"")) debug++;
-    
-            for (int p = 0; p <= 7; p++)
+            for (int p = 0; p < sizeof(priority_number); p++)
             {
-                char key[20];
-                snprintf(key, sizeof(key), "\"PRIORITY\":\"%d\"", p);
-                if (strstr(line, key)) {
-                    if (priority == p) {
-                        printf("%s", line);
-                    }
+                char key[24];
+                snprintf(key, sizeof(key),"\"PRIORITY\":\"%d\"", p);
+                if (strstr(line, key))
+                {
+                    priority_number[p]++;
                     break;
                 }
             }
+            if (log_counts[i] < MAX_LOGS)
+                logs[i][log_counts[i]++] = strdup(line);  
         }
-    
         fclose(fp);
-    
-        printf("\nServicio: %s\n", argv[i + 1]);
-        printf("  emerg  : %d\n", emerg);
-        printf("  alert  : %d\n", alert);
-        printf("  crit   : %d\n", crit);
-        printf("  err    : %d\n", err);
-        printf("  warning: %d\n", warning);
-        printf("  notice : %d\n", notice);
-        printf("  info   : %d\n", info);
-        printf("  debug  : %d\n", debug);
-        printf("--------------------------------------\n");
+        wait(NULL);
+
+        printf("║ %-21s ║", argv[i + 1]);
+        for (int p = 0; p < 8; p++) {
+            printf(" %s:%3d", all_priorities[p], priority_number[p]);
+            if (p < 7) printf(" |");
+        }
+        printf(" ║\n");
     }
-    
+
+    printf("╚═══════════════════════╩════════════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("Actualizado cada %d segundos. Ctrl+C para salir.\n", interval);
+
     for (int i = 0; i < num_services; i++)
     {
-        wait(NULL);
+        printf("Mensajes de %s\n", argv[1 + i]);
+        for (int l = 0; l < log_counts[i]; l++)
+        {
+            char* start = strstr(logs[i][l], "\"MESSAGE\":\"");
+            if (start)
+            {
+                start += strlen("\"MESSAGE\":\"");
+                char* end = strchr(start, '\"');
+                if (end)
+                {
+                    *end = '\0';
+                    printf("-%s\n", start);
+                }
+                
+            }
+        }
+        
     }
+    
+
+    sleep(interval);
 
     return 0;
 }
